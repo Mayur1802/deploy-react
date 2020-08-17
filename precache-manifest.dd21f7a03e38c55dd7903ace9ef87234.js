aadc34435b3ca033980882b12b291713799cb5ac
mayur <mayur93nemade@gmail.com>
2020-08-18 00:32:19 +0530

    Updates

---
+++ precache-manifest.dd21f7a03e38c55dd7903ace9ef87234.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77185cb114a18c5f0a1be0daebc2c82d",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "23ca32891a8bb34f40fd",
+    "url": "/deploy-react/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "56f383f67e8ebcd13064",
+    "url": "/deploy-react/static/css/main.aaf13e70.chunk.css"
+  },
+  {
+    "revision": "23ca32891a8bb34f40fd",
+    "url": "/deploy-react/static/js/2.229b8775.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/deploy-react/static/js/2.229b8775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f383f67e8ebcd13064",
+    "url": "/deploy-react/static/js/main.29aed659.chunk.js"
+  },
+  {
+    "revision": "c7361e32be76ffed8329",
+    "url": "/deploy-react/static/js/runtime-main.cea0da03.js"
+  },
+  {
+    "revision": "63792e7074d6d848cd9102cdcdbf322e",
+    "url": "/deploy-react/static/media/mayur.63792e70.jpeg"
+  }
+]);
